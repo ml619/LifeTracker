958b8de2bbd1aee2c95f8bbe32df6b4118d6cff4
Erin <73081649+ErinCohenGH@users.noreply.github.com>
2022-12-08 18:21:04 -0500

    Update ExportAvailability.cs

--- ExportAvailability.cs
+++ ExportAvailability.cs
@@ -1,49 +1,38 @@
-﻿public void ExportAvailability(Week week)
-{
-
-    // create a file to write to 
-    string fileName = @"C:\Documents\Availability.txt";
-    if (File.Exists(fileName))
-    {
-        File.Delete(fileName);
-    }
-
-
-   
-
-    using (StreamWriter sw = File.CreateText(fileName))
-    {
-
-        for (int i = 0; i <= 6; i++)
+    public void ExportAvailability() // Export times of events into text file
         {
-            List<string> days = new List<string>
-            { "Monday\n", "Tuesday\n", "Wednesday\n", "Thursday\n", "Friday\n", "Saturday\n", "Sunday\n" };
-
-            sw.WriteLine(days[i])
-
-            for (int j = 0; j < week[i].Count; j++)
+            // Create a file to write to (as applicable)
+            string fileName = "Availability.txt";
+            if (File.Exists(fileName))
             {
-                int epochSeconds = week[i][j].GetDate_Time();
-                int epochDuration = week[i][j].GetDuration();
-
-
-                DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(epochSeconds);
-                DateTime dateTime = dateTimeOffset.DateTime;
-                string startTime = dateTime.ToString("HH:mm");
-
-                int endtimeepoch = epochSeconds + epochDuration;
-                DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(endtimeepoch);
-                DateTime dateTime = dateTimeOffset.DateTime;
-                string endTime = dateTime.ToString("HH:mm");
-
-
-                sw.WriteLine(startTime + " to " + endTime + ",");
-    
-
+                File.Delete(fileName);
             }
+
+            // Write to file
+            using (StreamWriter sw = File.CreateText(fileName))
+            {
+                for (int i = 0; i <= 6; i++)
+                {
+                    List<string> days = new List<string>
+                    { "Monday\n", "\n\nTuesday\n", "\n\nWednesday\n", "\n\nThursday\n", "\n\nFriday\n", "\n\nSaturday\n", "\n\nSunday\n" };
+                    sw.WriteLine(days[i]);
+                    for (int j = 0; j < a_week[(i + 1) % 7].Count; j++)
+                    {
+                        long epochSeconds = a_week[(i + 1) % 7][j].GetDate_Time();
+                        long epochDuration = (long)(a_week[(i + 1) % 7][j].GetDuration() * 3600);
+
+                        DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(epochSeconds);
+                        DateTime dateTime = dateTimeOffset.DateTime;
+                        string startTime = dateTime.ToString("HH:mm");
+
+                        long endtimeepoch = epochSeconds + epochDuration;
+                        dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(endtimeepoch);
+                        dateTime = dateTimeOffset.DateTime;
+                        string endTime = dateTime.ToString("HH:mm");
+
+                        sw.WriteLine(startTime + " to " + endTime + ",");
+                    }
+                }
+            }
+            // Open file once written to
+            Process.Start(new ProcessStartInfo { FileName = fileName, UseShellExecute = true });
         }
-
-
-    }
-
-}
